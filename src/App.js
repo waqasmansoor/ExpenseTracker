3b4b3ccfe3879d41776cd06962f8975ed03bd936
Waqas Mansoor <waqasmansoor47@yahoo.com>
2020-07-14 18:51:34 +0500

    modified expense tracker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GlobalProvider from './context/GlobalContext';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
-import Transaction from './components/Transaction';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 
